6a801910865df12640427fde916b18d8de13480f
Grace de la Mora <grace.g.delamora@gmail.com>
2021-02-13 10:39:52 -0500

    Initializing project using Create React App

---
+++ README.md
@@ -0,0 +1,2 @@
+# dollblog
+A personal blog created using MERN, Zurb Foundation, and Font Awesome
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Fragment } from 'react';
+import './components/scss/app.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars);
+
+function App() {
+    return (
+      <Fragment>
+
+      </Fragment>
+    );
+  }
+
+export default App;
